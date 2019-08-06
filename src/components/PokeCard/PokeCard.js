738d12d7ef050523670039c169b53dc291933c6b
Mario <marioaslau@gmail.com>
2019-08-06 16:58:12 +0100

    feat: Tuesday-exercise

---
+++ src/components/PokeCard/PokeCard.js
@@ -0,0 +1,98 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { useNavigation } from 'react-navigation-hooks';
+
+import type { NavigationScreenProp } from 'react-navigation';
+
+type Pokemon = {
+  number: number,
+  name: string,
+  classification: string,
+  image: string,
+  types: [string],
+};
+
+type Props = {
+  navigation: NavigationScreenProp<any>,
+  pokemon: Pokemon,
+  index: number,
+  ghost: Boolean,
+};
+
+export function PokeCard(props: Props): React$Node {
+  const { navigation, pokemon, index } = props;
+
+  if(pokemon.name === ''){
+    return <View style={{    width: '100%', padding: 10,
+flex: 1}} />
+  }
+
+  return (
+    <View style={ [styles.container,index % 2 === 0 ? styles.containerEven : styles.containerUnEven] }>
+      <TouchableOpacity
+        accessibilityLabel="Go to pokemon details"
+        accessibilityComponentType="button"
+        accessibilityTraits="button"
+        onPress={() => navigation.navigate('Details', { pokemon })}
+        style={styles.itemRow}
+      >
+        <Text style={styles.title}>{pokemon.name}</Text>
+        <View style={styles.typeContainer}>
+          {
+            pokemon.types.map(type => <Text style={styles.type}>{type}</Text>)
+          }
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginBottom: 20,
+    width: '100%',
+    padding: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+  },
+  containerEven: {
+    marginRight: 8,
+  },
+  containerUnEven: {
+    marginLeft: 8,
+  },
+  itemRow: {
+    flex: 1,
+    height: 100,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '800',
+    letterSpacing: 2,
+  },
+  typeContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
+  type: {
+    backgroundColor: '#ebedf0',
+    display: 'flex',
+    padding: 5,
+    paddingRight: 15,
+    paddingLeft: 15,
+    borderRadius: 12,
+    flexShrink: 1,
+    alignSelf: 'flex-start',
+  },
+});
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,104 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { graphql, type OperationComponent } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { PokeCard } from '../components/PokeCard/PokeCard';
+// import { useNavigation } from 'react-navigation-hooks';
+
+import type { NavigationScreenProp } from 'react-navigation';
+
+type Props = {
+  navigation: NavigationScreenProp<any>,
+};
+
+type Pokemon = {
+  number: number,
+  name: String,
+  classification: string,
+  image: string,
+  types: [string],
+};
+
+const PokemonsQuery = gql`
+  query pokemons {
+    pokemons(first: 5) {
+      number
+      name
+      classification
+      image
+      types
+      evolutions {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+const emptyPokemon = {
+  number: 0,
+  name: '',
+  classification: '',
+  image: '',
+  types: [''],
+}
+
+// $FlowFixMe
+const getPokemonArray = (pokemons) => {
+  // console.log('POKE:', pokemons && pokemons.length)
+  let pokemon = pokemons;
+  if(pokemons && pokemon.length % 2 !== 0)
+  pokemon.push(emptyPokemon)
+  return pokemons
+}
+
+const keyExtractor = (item, index): string => item.number;
+
+ function HomeScreenComponent(props: Props): React$Node {
+
+  const { loading, error, pokemons } = props.data;
+  // console.log('here is my data', props.data.pokemons);
+  const checkedPokemon = getPokemonArray(pokemons);
+
+  const renderItem = ({ item, index }: { item: Object, index: number }): React$Node => {
+    return <PokeCard pokemon={item} navigation={props.navigation} index={index} />;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello this is the home screen</Text>
+
+
+      {loading? <Text>Loading...</Text>
+      : error? <Text>Error :</Text>
+      :
+      <FlatList
+              data={pokemons}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
+              numColumns={2}
+            />}
+    </View>
+  );
+}
+
+export const HomeScreen = graphql(PokemonsQuery)(HomeScreenComponent)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'coral',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
